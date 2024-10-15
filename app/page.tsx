bb0a55b0062703e8d0419f5756f3de1af97dcd9e
inushapeiris <inushaumayanthapeiris@gmail.com>
2024-10-15 14:48:53 +0530

    adding background theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ImageScrollAnimation from "./components/ImageScroller";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center bg-gray-900  text-white">
       <ImageScrollAnimation />
     </div>
   );
